61ab4cd1627a5baa1c72e247b5a6276874a68693
Jacob Anderson <andersonjwan@gmail.com>
2022-05-10 12:00:06 -0700

    feat: Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Bootstrap the set of dotfiles.
+# @author "Jacob Anderson <andersonjwan@gmail.com>"
+
+# Inspired by the following work:
+# https://github.com/mathiasbynens/dotfiles
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+# Pull the latest changes
+git pull origin main;
+
+# Update set of dotfiles and execute.
+function doIt() {
+    rsync --exclude ".git/" \
+	  --exclude "bootstrap.sh" \
+	  --exclude "README.md" \
+	  --archive --verbose --human-readable . ~;
+    source ~/.bash_profile;
+}
+
+# Bootstrap dotfiles.
+#
+# @option --force -f Force the boostrap process carelessly
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    doIt;
+else
+    read -p "This may overwrite existing files within your home directory. Are you sure (y/n) " -n 1;
+    echo "";
+
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+	doIt;
+    fi;
+fi;
